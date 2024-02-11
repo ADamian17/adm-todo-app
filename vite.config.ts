338bbf320b4d871a0dfad514db64da0b3ddf15cc
ADamian17 <adonismartin90@gmail.com>
2024-02-11 14:04:17 -0800

    added test env

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
-/** @type {import('vite').UserConfig} */
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./tests/setup.ts",
+  },
   css: {
     modules: {
       generateScopedName: "[name]__[local]___[hash:base64:5]",
