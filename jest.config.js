6a72cbad2abb18d8d98181eeb5be9dc94c69d8d2
Adonis D Martin <adonismartin90@gmail.com>
2024-02-11 15:20:49 -0800

    added jest @testing library (#1)

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('jest').Config} */
+
+export default {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["@testing-library/jest-dom"],
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+};
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,20 @@
-import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 
 import App from './App';
+
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
 
 describe('App', () => {
   it('renders headline', () => {
